18c304450b55223d5a247432ee22bdbe86f193d5
winston <1473457657@qq.com>
2025-11-09 13:27:43 +0800

    原型模式

---
+++ prototype/src/main/java/com/prototype/Client.java
@@ -0,0 +1,44 @@
+package com.prototype;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @ClassName: Client
+ * @Description: 测试用例
+ * @Author: Winston
+ * @Date: 2025/11/8 18:39
+ * @Version: 1.0
+ */
+public class Client {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        List<String> hobbies =  new ArrayList<>();
+        hobbies.add("singing");
+        hobbies.add("football");
+
+        Student original = new Student(20, hobbies, "张三");
+
+        Student clone = (Student)original.clone();
+
+        System.out.println(original.getAge()); // 20
+        System.out.println(original.getName()); // 张三
+        System.out.println(original.getHobbies());  // [singing, football]
+        System.out.println(clone.getHobbies() == original.getHobbies()); // true
+
+        clone.setName("李四");
+        clone.setAge(24);
+
+        clone.getHobbies().add("basketball");
+
+        // original
+        System.out.println(original.getAge()); // 20
+        System.out.println(original.getName()); // 张三
+        System.out.println(original.getHobbies()); // [singing, football, basketball]
+
+        // clone
+        System.out.println(clone.getAge()); // 24
+        System.out.println(clone.getName()); // 李四
+        System.out.println(clone.getHobbies()); // [singing, football, basketball]
+    }
+}
---
+++ prototype/src/main/java/com/prototype/Student.java
@@ -0,0 +1,63 @@
+package com.prototype;
+
+
+import java.util.List;
+
+/**
+ * @ClassName: Student
+ * @Description: 浅克隆测试
+ * @Author: Winston
+ * @Date: 2025/11/8 18:29
+ * @Version: 1.0
+ */
+public class Student implements Cloneable{
+
+    private int age; // 基本类型
+    private String name; // 不可变， 引用类型
+    private List<String> hobbies; // 可变的 引用类型
+
+    public Student(int age, List<String> hobbies, String name) {
+        this.age = age;
+        this.hobbies = hobbies;
+        this.name = name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public List<String> getHobbies() {
+        return hobbies;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "age=" + age +
+                ", name='" + name + '\'' +
+                ", hobbies=" + hobbies +
+                '}';
+    }
+
+    /**
+     * 克隆方法
+     * @return 返回克隆后的新对象
+     * @throws CloneNotSupportedException 异常处理
+     */
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+}
